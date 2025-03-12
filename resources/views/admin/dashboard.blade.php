c2c3ab984db7042012ee3fc7e53a276295f51163
Irving R. <riosirving04@gmail.com>
2025-03-11 21:08:25 -0600

    migraciones y creacion de base de datos

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,31 @@
+<x-admin-layout :breadcrumb="[
+    [
+        'name' => 'Dashboard',
+        'url' => '#'
+    ],
+    [
+        'name' => 'Projects',
+        
+    ]
+]">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, molestias! Exercitationem libero eius consectetur nisi explicabo pariatur itaque molestiae, possimus in, aliquam neque ullam expedita praesentium qui dolores eos incidunt!</p>
+    <p>Sunt nulla cumque ex, officiis eaque quos eveniet, vero, sequi quod at ipsum! Exercitationem ex et facere saepe quos mollitia eaque. Corporis rerum commodi, et quaerat eveniet voluptate. Ipsum, quidem.</p>
+    <p>Maxime mollitia reprehenderit nam perferendis facilis modi voluptates, optio esse consequuntur facere, autem aspernatur quas sit illo, placeat est repellat explicabo quasi! Animi similique, unde earum dolores quod accusantium. Libero.</p>
+    <p>Reiciendis, qui nulla sit eos inventore suscipit consectetur molestiae recusandae tempore vitae reprehenderit laboriosam alias, aliquid placeat officia rerum dolorem, quos aut distinctio commodi optio. Odio nostrum tempore amet necessitatibus.</p>
+    <p>Non, earum? Atque laborum, perspiciatis harum vitae in repudiandae. Dolorem esse at eligendi rerum deleniti nam consequuntur cumque ducimus animi perferendis accusantium, a tenetur minima alias, obcaecati expedita ratione tempore.</p>
+    <p>Numquam odit dolores eveniet, velit possimus aperiam pariatur culpa veniam officiis consequuntur placeat ut illo nobis at nulla tenetur ratione nostrum architecto ab quasi iste eum nisi libero. Nostrum, sapiente?</p>
+    <p>Quibusdam rem voluptates officia, odit illum, voluptas repellendus facilis culpa saepe labore laboriosam alias doloribus aliquam tenetur odio. Architecto quod incidunt aliquid, eum quis cum porro asperiores accusantium magni. Cumque.</p>
+    <p>Earum asperiores, incidunt harum dolor non, laudantium repellat vel sapiente aperiam qui temporibus, aliquid iusto velit cumque. Laborum facilis consectetur, magni voluptatibus aperiam optio eligendi animi perspiciatis vitae quo voluptate.</p>
+    <p>Maiores velit dolorum, sequi veniam adipisci hic nam vel odio dolorem alias, distinctio officia suscipit, laborum eligendi odit saepe neque unde. Voluptatum sunt similique dicta a sit nesciunt omnis tempore.</p>
+    <p>Nam optio nostrum dolore molestiae commodi fugiat temporibus corporis suscipit cupiditate excepturi eos dicta nemo repudiandae provident in, sequi amet asperiores! Ratione quo, ipsa aliquid nisi ea dignissimos nostrum qui.</p>
+    <p>Aliquam, dignissimos iusto dolor numquam vero cumque ullam ipsum sapiente quia eveniet ipsa velit magni a mollitia consectetur corrupti dicta at cupiditate consequatur sed perspiciatis. Dolor animi voluptatem corrupti vitae.</p>
+    <p>Velit provident minima cum ullam quia ducimus voluptate aspernatur ea dolorem quos praesentium illo nemo rerum recusandae sit voluptatem hic placeat amet eaque, alias, quasi explicabo sequi ratione error. Inventore!</p>
+    <p>In aut excepturi amet sunt. Natus, nemo iusto! Esse quis eaque odio asperiores molestiae ea perferendis, reiciendis quas accusantium illum unde optio! Recusandae omnis vel similique cumque, provident amet modi?</p>
+    <p>Doloribus nam facilis ipsum qui consequuntur, alias fuga ex pariatur magni assumenda vel. Recusandae illum facere cum ut. Molestiae nisi quo iusto aperiam itaque modi minima blanditiis quae. Voluptate, esse!</p>
+    <p>Pariatur culpa quisquam deleniti voluptates odit fugiat id unde aspernatur dolores ad aliquam minus quibusdam facere adipisci nam voluptatibus sunt, fugit officiis earum ipsa libero rerum accusamus tempore voluptatem? Iure!</p>
+    <p>Distinctio, fugiat sed est neque id sit iusto? Enim provident repudiandae est eum eaque labore corporis ut vitae molestias qui illum reprehenderit, molestiae consectetur autem repellat quis at praesentium rerum.</p>
+    <p>Illo, velit. Cupiditate deserunt eius quia nisi delectus architecto enim tempore aliquid, alias veniam qui dignissimos blanditiis perferendis totam accusantium aspernatur asperiores eos commodi ut saepe doloribus iusto est. Blanditiis.</p>
+    <p>Eos vero molestias rerum impedit corporis soluta labore, quaerat doloremque quisquam distinctio animi consequatur omnis iusto ut atque optio possimus velit necessitatibus ipsa officiis minima debitis. At ea voluptatum id!</p>
+    <p>Labore facilis expedita ut, quisquam molestiae omnis id dolores aliquam itaque. Dolores, ullam commodi vel, aut animi sequi laboriosam repellendus eligendi ducimus eum, dignissimos corporis nisi! Optio sapiente eius asperiores!</p>
+    <p>Dolorem totam quae odit aspernatur at minima, officiis esse. Molestias ipsum, alias impedit deleniti ducimus earum magnam ratione, aut vel vitae, labore voluptates distinctio iste accusantium fugit eius id provident.</p>     
+</x-admin-layout>
---
+++ resources/views/instructor/dashboard.blade.php
@@ -0,0 +1,1 @@
+<x-instructor-layout></x-instructor-layout>
